d26b3250bf95b334adae6d5ed84757e6e2ebec5d
Haoliang Yu <hyu@esri.com>
2019-06-27 16:39:49 -0400

    fix request query bug and enhance error handling

--- src/index.js
+++ src/index.js
@@ -6,15 +6,20 @@ function Output() {}
 Output.prototype.serve = function handleRequest(req, res) {
   this.model.pull(req, (error, geojson) => {
     if (error) {
-      return res.status(err.code || 500).json({ error });
+      return res.status(error.code || 500).json({ message: error.message });
     }
 
     // send data to winnow; filter the data according to query
-    const query = req.body || req.query;
+    const query = req.method === "POST" ? req.body : req.query;
     const options = Object.assign({ toEsri: false }, query);
-    const result = winnow.query(geojson, options);
+    let result;
 
-    res.status(200).json(result);
+    try {
+      result = winnow.query(geojson, options);
+      res.status(200).json(result);
+    } catch (error) {
+      res.status(error.code || 500).json({ message: error.message });
+    }
   });
 };
 
